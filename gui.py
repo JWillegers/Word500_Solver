1dd99a3f9fc6024679ff3cc8fcaaf5d731455e7d
Jonathan <148167.jw@gmail.com>
2022-11-09 13:54:01 +0100

    GUI main screen as frame

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+
 import prepare_game
 
 #change these number for changing screen size
@@ -15,22 +16,28 @@ input_color = '#2C3639'
 color4 = '#DCD7C9'
 
 def run():
-    window = tk.Tk(className='Word500 solver by JWillegers') #create window
+    window = tk.Tk(className='Word500 solver by JWillegers')  # create window
     window.geometry(str(width) + 'x' + str(height))
     window.resizable(False, False)
     window.configure(bg=bg_color)
 
-    home_screen()
+    home_screen(window)
     window.mainloop() #show window (and interact with it)
 
-def home_screen():
-    greeting = tk.Label(text='Word500 solver by JWillegers', font=('Arial', int(height / 20)), fg=txt_color, bg=bg_color)
-    greeting.place(relx=0.5, rely=0.1, anchor='center')  # add to window
 
-    select = tk.Label(text='Start by selecting a difficulty', font=('Arial', int(height / 40)), fg=txt_color, bg=bg_color)
-    select.place(relx=0.5, rely=0.25, anchor='center')  # add to window
+def home_screen(window):
+    global middle_frame
+    middle_frame = tk.Frame(window, bg=bg_color)
+    middle_frame.pack()
+
+    greeting = tk.Label(middle_frame, text='Word500 solver by JWillegers', font=('Arial', int(height / 20)), fg=txt_color, bg=bg_color)
+    greeting.pack(pady=int(height/50))
+
+    select = tk.Label(middle_frame, text='Start by selecting a difficulty', font=('Arial', int(height / 40)), fg=txt_color, bg=bg_color)
+    select.pack(pady=int(height/50))
 
     button_easy = tk.Button(
+        middle_frame,
         text='Easy',
         width=20,
         height=2,
@@ -39,9 +46,10 @@ def home_screen():
         fg=color4,
         bg=input_color
     )
-    button_easy.place(relx=0.5, rely=0.4, anchor='center')
+    button_easy.pack(pady=int(height/50))
 
     button_medium = tk.Button(
+        middle_frame,
         text='Medium',
         width=20,
         height=2,
@@ -50,8 +58,9 @@ def home_screen():
         fg=color4,
         bg=input_color
     )
-    button_medium.place(relx=0.5, rely=0.55, anchor='center')
+    button_medium.pack(pady=int(height/50))
     button_hard = tk.Button(
+        middle_frame,
         text='Hard',
         width=20,
         height=2,
@@ -60,45 +69,29 @@ def home_screen():
         fg=color4,
         bg=input_color
     )
-    button_hard.place(relx=0.5, rely=0.7, anchor='center')
-
-    button_rule_1 = tk.Label(
-        text='- No JQXZ',
-        font=('Arial', int(height / 70)),
-        fg=color4,
-        bg=bg_color
-    )
-    button_rule_1.place(relx=0.6, rely=0.37)
-
-    button_rule_2 = tk.Label(
-        text='- No repeat letters',
-        font=('Arial', int(height / 70)),
-        fg=color4,
-        bg=bg_color
-    )
-    button_rule_2.place(relx=0.6, rely=0.4)
-
-    button_rule_3 = tk.Label(
-        text='- No repeat letters',
-        font=('Arial', int(height / 70)),
-        fg=color4,
-        bg=bg_color
-    )
-
-    button_rule_3.place(relx=0.6, rely=0.53)
+    button_hard.pack(pady=int(height/50))
 
 
 def easy():
     start_game('easy')
 
+
 def medium():
     start_game('medium')
+
 
 def hard():
     start_game('hard')
 
+
 def start_game(difficulty):
     allowed_words, words_still_possible = prepare_game.get_words(difficulty)
+    build_game_screen()
 
+
+def build_game_screen():
+    #first remove all old widgets
+    for widget in middle_frame.winfo_children():
+        widget.destroy()
 
 run()
